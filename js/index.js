4de2256989dec852306a138eddc5796c1a0a2ae2
songziyu <1451664712@qq.com>
2019-06-25 15:38:58 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
-$('#menu').click(() => {
-    $('.mask').toggleClass('addWidth');
+$(function () {
+    $('#menu').click(() => {
+        $('.mask').toggleClass('addWidth');
+    })
+    $('#close').click(() => {
+        $('.mask').removeClass('addWidth')
+    })
+    var oWidth = document.querySelectorAll('.gallery_block>ul>li')[0].offsetWidth
+    var aWidth = document.querySelectorAll('.gallery_controls>ul>li')[0].offsetWidth
+    var list = document.querySelectorAll('.gallery_controls>ul>li')
+    var oUl = document.querySelector('.gallery_block>ul')
+    var oSpan = document.querySelectorAll('.gallery_controls>ul>li>span')
+    console.log(aWidth);
+    list.forEach((item, index) => {
+        item.onclick = function () {
+            for (var i = 0; i < list.length; ++i) {
+                list[i].classList.remove("hover")
+            }
+            this.classList.add("hover")
+            oUl.style.left = `-${index * oWidth}` + 'px'
+        }
+    })
 })
-$('#close').click(() => {
-    $('.mask').removeClass('addWidth')
-})
-let List = $('.gallery_controls ul li')
-let content = $('.gallery_block ul li')
-console.log(content);
-List.map((index, item) => {
-    item.onclick = function () {
-        console.log(1)
-       
-    }
-})
+var aa = document.querySelectorAll('polyline')
+console.log(aa);
