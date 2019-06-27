38b2ac63f121ec4d33cf7f9df5599b8952735a2c
songziyu <1451664712@qq.com>
2019-06-27 13:55:37 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# demo
+<!-- # demo
 
+    // 清除所有小图片样式
+    var clearClass = () => {
+        list.forEach((item) => {
+            item.classList.remove("hover")
+        })
+        oUl.style.left = `-${_index * oWidth}` + 'px'
+        list[_index].classList.add("hover")
+    }
+
+    var oLeftBtn = document.querySelector('.left');
+    var oRightBtn = document.querySelector('.right');
+    var oLength = document.querySelectorAll('.gallery_block>ul>li').length;
+    oLeftBtn.onclick = () => {
+        left()
+    }
+    oRightBtn.onclick = () => {
+        right()
+    }
+
+    left = () => {
+        _index--
+        if (_index < 0) {
+            _index = oLength - 1
+        }
+        clearClass()
+
+    }
+    right = () => {
+        _index++
+        if (_index > oLength - 1) {
+            _index = 0
+        }
+        clearClass()
+    }
+    // 定时器
+    // var timer = null;
+    // timer = setInterval(function () {
+    //     right()
+    // }, 1000)
+    // var oBox = document.querySelector('.gallery_block')
+    // oBox.onmouseover = () => {
+    //     clearInterval(timer)
+    // }
+    // oBox.onmouseout = () => {
+    //     timer = setInterval(function () {
+    //         right()
+    //     }, 2000)
+    // } -->
--- js/index.js
+++ js/index.js
@@ -1,16 +1,4 @@
 $(function () {
-    // for (var i = 0; i < list.length; i++) {
-    //     (function (index) {
-    //         list[index].onclick = function () {
-    //             for (var i = 0; i < list.length; i++) {
-    //                 list[i].classList.remove("hover")
-    //             }
-    //             this.classList.add("hover")
-    //             oUl.style.left = `-${index * oWidth}` + 'px'
-    //             console.log(index);
-    //         }
-    //     })(i)
-    // }
     $('#menu').click((e) => {
         if (e && e.stopPropagation) {
             e.stopPropagation();
@@ -27,65 +15,63 @@ $(function () {
         }
         $('.mask').removeClass('addWidth')
     })
-    var oWidth = document.querySelectorAll('.gallery_block>ul>li')[0].offsetWidth
-    var list = document.querySelectorAll('.gallery_controls>ul>li')
-    var oUl = document.querySelector('.gallery_block>ul')
+    var oLi = document.querySelectorAll('.gallery_block>ul>li')
+    var oList = document.querySelectorAll('.gallery_controls>ul>li')
     var _index = 0;
-    list.forEach((item, index) => {
+    oList.forEach((item, index) => {
         item.onclick = function () {
-            clearClass()
+            removeName()
             this.classList.add("hover")
-            oUl.style.left = `-${index * oWidth}` + 'px'
+            oLi[index].classList.add("list")
             _index = index
         }
     });
 
-    // 清除所有小图片样式
-    var clearClass = () => {
-        list.forEach((item) => {
-            item.classList.remove("hover")
-        })
-        oUl.style.left = `-${_index * oWidth}` + 'px'
-        list[_index].classList.add("hover")
+    removeName = () => {
+        for (var i = 0; i < oList.length; i++) {
+            oList[i].classList.remove("hover")
+            oLi[i].classList.remove("list")
+        }
     }
-
-    var oLeftBtn = document.querySelector('.left');
-    var oRightBtn = document.querySelector('.right');
     var oLength = document.querySelectorAll('.gallery_block>ul>li').length;
-    oLeftBtn.onclick = () => {
-        left()
+    var btnLeft = document.querySelector('.left')
+    var btnRight = document.querySelector('.right')
+    var oContent = document.querySelector('.gallery_block')
+    var timer = null
+    btnLeft.onclick = () => {
+        leftMove()
     }
-    oRightBtn.onclick = () => {
-        right()
+    btnRight.onclick = () => {
+        rightMove()
     }
-
-    left = () => {
+    leftMove = () => {
+        removeName();
         _index--
         if (_index < 0) {
             _index = oLength - 1
         }
-        clearClass()
-
+        oLi[_index].classList.add("list")
+        oList[_index].classList.add("hover")
     }
-    right = () => {
-        _index++
-        if (_index > oLength - 1) {
+    rightMove = () => {
+        removeName();
+        _index++;
+        if (_index == oLength) {
             _index = 0
         }
-        clearClass()
+        oLi[_index].classList.add("list")
+        oList[_index].classList.add("hover")
     }
-    // 定时器
-    // var timer = null;
-    // timer = setInterval(function () {
-    //     right()
-    // }, 1000)
-    // var oBox = document.querySelector('.gallery_block')
-    // oBox.onmouseover = () => {
-    //     clearInterval(timer)
-    // }
-    // oBox.onmouseout = () => {
-    //     timer = setInterval(function () {
-    //         right()
-    //     }, 2000)
-    // }
+    function time() {
+        timer = setInterval(() => {
+            rightMove();
+        }, 2000)
+    }
+    time()
+    oContent.onmouseover = () => {
+        clearInterval(timer)
+    }
+    oContent.onmouseout = () => {
+        time()
+    }
 })
